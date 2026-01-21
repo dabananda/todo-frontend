12b8b9e38b0f4593d75faf2aeb470a96a86c6ff1
dabananda <dabananda.dev@gmail.com>
2026-01-21 10:38:24 +0600

    feat: api endpoints added for fetching

---
+++ src/services/TodoService.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const API_TODO_BASE_URL = 'http://localhost:8080/api/todos'
+
+export const getTodos = () => axios.get(API_TODO_BASE_URL)
+export const getTodoById = (id) => axios.get(API_TODO_BASE_URL + '/' + id)
+export const createTodo = (todo) => axios.post(API_TODO_BASE_URL, todo)
+export const updateTodo = (id, todo) => axios.put(API_TODO_BASE_URL + '/' + id, todo)
+export const deleteTodo = (id) => axios.get(API_TODO_BASE_URL + '/' + id)
+export const completeTodo = (id) => axios.get(API_TODO_BASE_URL + '/' + id + '/complete')
+export const inCompleteTodo = (id) => axios.get(API_TODO_BASE_URL + '/' + id + '/in-complete')
