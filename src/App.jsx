aff49c822c1378d052efa20625509f7eaeeab9f2
dabananda <dabananda.dev@gmail.com>
2026-01-21 10:48:04 +0600

    feat: primary routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap/dist/js/bootstrap.min.js"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import Homepage from './pages/Homepage'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={<Homepage />}
+        />
+        <Route
+          path="/todos"
+          element={<Homepage />}
+        />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
